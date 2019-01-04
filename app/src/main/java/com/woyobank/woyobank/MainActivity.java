36c44ed5c3b6d82d65febd774abb07635e20a0d7
Ilhamw <ilhamwihardja@gmail.com>
2019-01-04 15:08:13 +0800

    update main
    add Intent to other activity

--- app/src/main/java/com/woyobank/woyobank/MainActivity.java
+++ app/src/main/java/com/woyobank/woyobank/MainActivity.java
@@ -1,13 +1,22 @@
 package com.woyobank.woyobank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void balanceButton(View view){
+        Intent balanceIntent = new Intent(MainActivity.this, CheckBalance.class);
+        MainActivity.this.startActivity(balanceIntent);
     }
 }
