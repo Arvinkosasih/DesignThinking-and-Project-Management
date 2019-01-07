684cfc4243b910692c8f488b7ca9f339bdc8d118
DBlackFox <arvin.kosasih@my.jcu.edu.au>
2019-01-07 13:21:41 +0800

    layout
    All layout

---
+++ loginPage/app/src/main/java/com/example/arvin/loginpage/signupPage.java
@@ -0,0 +1,13 @@
+package com.example.arvin.loginpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signupPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_page);
+    }
+}
